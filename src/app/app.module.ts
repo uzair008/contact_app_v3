86db63a5ce5720c4c762aaa42b1a2833d2bb3141
Sandhiya Lal <sandhiya.lal@einfochips.com>
2017-11-28 17:48:18 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Contacts } from '@ionic-native/contacts';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +23,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Contacts,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { Contacts } from '@ionic-native/contacts';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  contactlist = [];
+  testCheckboxOpen = false;
+  testCheckboxResult: any;
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public platform: Platform, public contacts: Contacts) {
+    this.platform.ready().then(() => {
+      this.contacts.find(['displayName','emails'], {filter: "", multiple: true}).then((contacts) => {
+      for(let i = 0; i < contacts.length; i++){
+        let contact=contacts[i];
+        if(contact.emails != null){
+          this.contactlist.push(contact);
+        }
+      }
+        console.log('Contacts'+this.contactlist);
+      }, (error) => {
+        console.log(error);
+      })
+   })
   }
 
+  doCheckbox() {  
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Select Contacts To Send Invitation');
+    for(let contact of this.contactlist){
+        alert.addInput({
+        type: 'checkbox',
+        label: contact.displayName,
+        value: contact.emails
+      });
+    }
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Submit',
+      handler: (data: any) => {
+        console.log('Checkbox data:', data);
+        this.testCheckboxOpen = false;
+        this.testCheckboxResult = data;
+      }
+    });
+    alert.present();
+  }
 }
